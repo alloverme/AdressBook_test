9237483f0f8855accbf6dee236438563b3662320
alloverme <ramzes55@yandex.ru>
2017-07-13 01:13:56 +0300

    bugs fixed, added surename

--- main.py
+++ main.py
@@ -30,8 +30,11 @@ class AddressBook(object):
             self.notes.append(Note(note))
 
     def show_users(self):
-        for key, note in enumerate(self.notes):
-            print str(key) + ' ' + str(note)
+        if len(self.notes) == 0:
+            print u'Your adress book is empty :( '
+        else:
+            for key, note in enumerate(self.notes):
+                print str(key) + ' ' + str(note)
 
     def add_user(self):
         new_note = Note()
@@ -47,6 +50,13 @@ class AddressBook(object):
                 break
             else:
                 print u'Make sure that you are using only letter and your Name length less that 10 digits'
+        while True:
+            input_str = raw_input(u'Add a surename\n')
+            if input_str.isalpha() and len(input_str) < 15:
+                new_note.surename = input_str
+                break
+            else:
+                print u'Make sure that you are using only letter and your Surename length less that 10 digits'
         while True:
             input_str = raw_input(u'Add a phone number without +\n')
             if input_str.isdigit() and len(input_str) == 11:
@@ -73,27 +83,32 @@ class AddressBook(object):
         store_data(self.notes)
 
     def delete_note(self, idx):
-        del self.notes[int(idx)]
-        store_data(self.notes)
+        try:
+            del self.notes[int(idx)]
+            store_data(self.notes)
+        except:
+            print u'There is no such contact!'
 
 
 class Note(object):
 
     def __init__(self, initial=None):
         self.name = ''
+        self.surename = ''
         self.phone_number = ''
         self.birth_date = ''
 
         if initial:
             self.name = initial['name']
+            self.surename = initial['surename']
             self.phone_number = initial['phone_number']
             self.birth_date = initial['birth_date']
 
     def __str__(self):
-        return 'Name: ' + self.name + ' Phone number: ' + self.phone_number + ' Birth date: ' + self.birth_date
+        return 'Name: ' + self.name + ' |Surename: ' + self.surename + ' |Phone number: ' + self.phone_number + ' |Birth date: ' + self.birth_date
 
     def __repr__(self):
-        return '{"name":"' + self.name + '","phone_number":"' + self.phone_number + '","birth_date":"' + self.birth_date + '"}'
+        return '{"name":"' + self.name +  '","surename":"' + self.surename + '","phone_number":"' + self.phone_number + '","birth_date":"' + self.birth_date + '"}'
 
 
 if __name__ == '__main__':
@@ -116,6 +131,12 @@ Choose the option
         elif user_input == '3':
             new_user.show_users()
             new_user.delete_note(raw_input('Select a contact by a number\n'))
+            # print ('Select a contact by a number\n')
+            # if (raw_input()> u'0'):
+            #     new_user.delete_note(raw_input)
+            # else:
+            #     print ("There is no such contact! Make sure you entered nonnegative number\n") # Exclude the possibility of deleting from the list by reverse idx
+            new_user.show_users()
         elif user_input == '4':
             break
         else:
